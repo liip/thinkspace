383843315240620700a763336115483061ac475b
Benoît Burgener <benoit.burgener@gmail.com>
2020-11-12 17:14:41 +0100

    Fix Contact classes being wrongly purged

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,11 +2,11 @@
   <div class="container max-w-6xl mt-4">
     <div
       class="p-6 lg:p-8 xl:p-10 relative rounded-lg lg:rounded-xl shadow-lg lg:shadow-xl md:flex items-center"
-      :class="`bg-${color}-500`"
+      :class="colors.bg"
     >
       <div class="mb-4 md:mb-0 md:mr-12 md:flex-1">
         <h2 class="text-white mb-3">{{ title }}</h2>
-        <p v-if="body" class="text-medium" :class="`text-${color}-100`">
+        <p v-if="body" class="text-medium" :class="colors.text">
           {{ body }}
         </p>
       </div>
@@ -36,6 +36,18 @@ export default {
     body: {
       type: String,
       default: null,
+    },
+  },
+
+  computed: {
+    colors() {
+      switch (this.color) {
+        case 'red':
+          return { bg: 'bg-red-500', text: 'text-red-100' };
+        case 'green':
+        default:
+          return { bg: 'bg-green-500', text: 'text-green-100' };
+      }
     },
   },
 };
